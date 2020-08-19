2a61fcea23ab1f7ac02ec55974a86418363e35d1
orol123456 <orol123456@naver.com>
2020-08-19 18:05:28 +0900

    seceret information made

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express=require('express')
 const app=express()
 const port=5000
+const bodyParser=require('body-parser')
 const moongose = require('mongoose')
-moongose.connect('mongodb+srv://jonghyuk:abcd1234@boilerplate.kcjzj.mongodb.net/<dbname>?retryWrites=true&w=majority',{
-useUnifiedTopology:true,useCreateIndex: true, useFindAndModify:false}).then(()=>console.log('mongodb connected..')).catch(err=>console.log(err))
+const {User}=require('./models/User')
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+moongose.connect(config.mongoURI,{useUnifiedTopology:true,useCreateIndex: true, useFindAndModify:false}).then(()=>console.log('mongodb connected..')).catch(err=>console.log(err))
 app.get('/',(req,res)=>res.send('hi'))
-app.listen(port,()=>console.log(`app listening at${port}`))
+app.post('/register',(req,res)=>{
+    const user=new User(req.body)
+    user.save((err,doc)=>{
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
+app.listen(port,()=>console.log(`application listening at${port}`))
